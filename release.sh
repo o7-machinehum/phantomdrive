d5df95a4ef1583267796d0017dfec21a4a54820a
o7-machinehum <ryan.cjw@gmail.com>
2026-07-22 23:33:08 +0200

    Release scipt.

--- release.sh
+++ release.sh
@@ -7,3 +7,8 @@ make AES_MODE=CTR KDF_ROUNDS=100000
 make AES_MODE=CTR KDF_ROUNDS=600000
 make AES_MODE=XTS KDF_ROUNDS=100000
 make AES_MODE=XTS KDF_ROUNDS=600000
+
+cp build/CTR_100K/Phantomdrive_MSC.elf build/phantomdrive_ctr_100k.elf
+cp build/CTR_600K/Phantomdrive_MSC.elf build/phantomdrive_ctr_600k.elf
+cp build/XTS_100K/Phantomdrive_MSC.elf build/phantomdrive_xts_100k.elf
+cp build/XTS_600K/Phantomdrive_MSC.elf build/phantomdrive_xts_600k.elf
